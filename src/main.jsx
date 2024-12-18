d045d78ceecd056997602292a5afdc576ee9a631
kira <chitimellapraneeth@gmail.com>
2024-12-18 19:44:56 +0530

    added my-trips functionality

--- src/main.jsx
+++ src/main.jsx
@@ -9,23 +9,28 @@ import Header from './components/custom/Header.jsx'
 import { Toaster } from './components/ui/sonner.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import Viewtrip from './view-trip/[tripId]/index.jsx'
+import MyTrips from './my-trips/index.jsx'
 
 
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element:<App/>
+    path: "/",
+    element: <App />,
   },
   {
-    path:'/create-trip',
-    element:<CreateTrip/>
+    path: "/create-trip",
+    element: <CreateTrip />,
   },
   {
-    path:'view-trip/:tripId',
-    element:<Viewtrip/>
-  }
-])
+    path: "view-trip/:tripId",
+    element: <Viewtrip />,
+  },
+  {
+    path: "/my-trips",
+    element: <MyTrips />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
